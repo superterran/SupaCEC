3e4fcae1869e20050bd23f18c782f48fb081b47b
Doug Hatcher <superterran@gmail.com>
2017-06-18 21:45:51 -0400

    Update supacec.sh

--- RetroPie-Setup/scriptmodules/supplementary/supacec.sh
+++ RetroPie-Setup/scriptmodules/supplementary/supacec.sh
@@ -10,7 +10,7 @@
 #
 
 rp_module_id="supacec"
-rp_module_desc="HDMI-CEC Awareness"
+rp_module_desc="Adds some much needed HDMI-CEC awareness, hopefully this package will drum up some desire for the feature and some better developers will help flesh this out. PRs are accepted! At the moment, this will send a CEC switch input command when new input devices are discovered in /dev/input i.e. turning on and pairing a controller."
 rp_module_licence="GPL3"
 rp_module_section="exp"
 rp_module_flags="!x11"
@@ -20,12 +20,19 @@ function depends_supacec() {
 }
 
 function sources_supacec() {
-    gitPullOrClone "$md_build" "https://github.com/superterran/SupaCEC.git"
+    gitPullOrClone "$md_build" "https://github.com/superterran/SupaCEC.git" "master"
 
 }
 
 function install_supacec() {
-}
+    echo $md_build
+    cd "$md_build"
+    echo "dropping some files..." 
+    sudo cp "$md_build/supacec.d" /etc/init.d/supacec
+    sudo cp "$md_build/supacec" /usr/bin/supacec
+    sudo chmod +x /usr/bin/supacec
+    sudo chmod +x /etc/init.d/supacec
+    sudo update-rc.d supacec defaults
+    sudo /etc/init.d/supacec start
 
-function configure_supacec() {
 }
